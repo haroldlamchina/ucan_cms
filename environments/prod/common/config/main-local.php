574b25c2e2169638c771d51ae7095c0fcb1491a3
haroldlam <455727503@qq.com>
2017-02-22 16:04:43 +0800

    v1.0   OS YII2 2.0.11    install

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=localhost;dbname=ucan_cms',
+            'username' => 'ucan_cms',
+            'password' => '123456',
             'charset' => 'utf8',
         ],
         'mailer' => [
