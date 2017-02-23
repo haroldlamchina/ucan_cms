0a5f1f15b51d1b22e59602b445ea7c6e9723ee98
haroldlam <455727503@qq.com>
2017-02-23 11:26:06 +0800

    v1.0.2   commentstatus poststatus   insert default values;

--- console/controllers/InitController.php
+++ console/controllers/InitController.php
@@ -41,4 +41,34 @@ class InitController extends Controller
         echo "Create user sucessfull!\n";
         return 0;                                       // 返回0表示一切OK
     }
+
+
+    /**
+     * Create init adminuser
+     */
+    public function actionAdminuser()
+    {
+        echo "Create init adminuser ...\n";                  // 提示当前操作
+        $username = $this->prompt('adminuser Name:');        // 接收用户名
+        $email = $this->prompt('Email:');               // 接收Email
+        $password = $this->prompt('Password:');         // 接收密码
+        $model = new User();                            // 创建一个新用户
+        $model->username = $username;                   // 完成赋值
+        $model->email = $email;
+        $model->password = $password;
+        $model->generateAuthKey();                     //自动创建auth_key
+        if (!$model->save())                            // 保存新的用户
+        {
+            foreach ($model->getErrors() as $error)     // 如果保存失败，说明有错误，那就输出错误信息。
+            {
+                foreach ($error as $e)
+                {
+                    echo "$e\n";
+                }
+            }
+            return 1;                                   // 命令行返回1表示有异常
+        }
+        echo "Create adminuser sucessfull!\n";
+        return 0;                                       // 返回0表示一切OK
+    }
 }
