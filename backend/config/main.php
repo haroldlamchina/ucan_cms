bc17e57b2b8006cc7c17ebcdcc177ba54ebdc0d7
haroldlam <455727503@qq.com>
2017-05-02 13:57:14 +0800

    增加adminuser验证登录

--- backend/config/main.php
+++ backend/config/main.php
@@ -11,13 +11,36 @@ return [
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
+    /*'modules' => [
+        'admin' => [
+            'class' => 'mdm\admin\Module',
+            //'layout' => 'left-menu',//yii2-admin的导航菜单
+        ],
+    ],*/
     'modules' => [
         'admin' => [
             'class' => 'mdm\admin\Module',
-            'layout' => 'left-menu',//yii2-admin的导航菜单
+            'layout' => 'left-menu',
+            'controllerMap' => [
+                'assignment' => [
+                    'class' => 'mdm\admin\controllers\AssignmentController',
+                    //'userClassName' => 'common\models\User',
+                    'userClassName' => 'common\models\Adminuser',
+                    'idField' => 'id'
+                ]
+            ],
+            'menus' => [
+                'assignment' => [
+                    'label' => 'Grand Access' // change label
+                ],
+                //'route' => null, // disable menu route
+            ]
+        ],
+        'debug' => [
+            'class' => 'yii\debug\Module',
         ],
     ],
-    'components' => [
+    /*'components' => [
         'request' => [
             'csrfParam' => '_csrf-backend',
         ],
@@ -57,6 +80,38 @@ return [
             'class' => 'yii\rbac\DbManager', // 使用数据库管理配置文件
         ],
 
+    ],*/
+    'components' => [
+        // 'user' => [//前台用户组件，后台用不到
+        //     'identityClass' => 'common\models\User',
+        //     'enableAutoLogin' => true,
+        // ],
+        'user' => [
+            //管理员
+            'class' => '\yii\web\User',
+            'loginUrl' => array('/site/login'),//没有登录就跳转到
+            'idParam'           => '_aId',
+            'identityCookie'    => ['name'=>'_aa','httpOnly' => true],
+            'identityClass' => 'common\models\Adminuser',
+            'enableAutoLogin' => true,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        //rdbc
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+            //'defaultRoles' => ['guest'],
+        ],
     ],
     'params' => $params,
     'aliases' => [
